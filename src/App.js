b7befb33b585921e461a045e8295b5a61cbe49fd
rock's ezweb <alikerock@gmail.com>
2024-06-10 23:59:34 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useEffect, useState, useCallback  } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import Todo from './Todo';
 
 import Button from 'react-bootstrap/Button';
@@ -11,83 +11,85 @@ function App() {
   //let test = window.localStorage.getItem('name')
   //const personObj = JSON.parse(test);  문자열 -> json 형식 객체 변환
 
-  const[todo,setTodo] = useState([]);
+  const [todo, setTodo] = useState([]);
   let [todoid, setTodoid] = useState(0);
 
-  const getTodoList = ()=>{
+  const getTodoList = useCallback(() => {
     const todoListFromStorage = window.localStorage.getItem('todo');
     //console.log(todoListFromStorage);
-    if(todoListFromStorage !== null && todoListFromStorage !=='[]'){ //값이 있으면
-      const todoObj = JSON.parse(todoListFromStorage);  
+    if (todoListFromStorage !== null && todoListFromStorage !== '[]') { //값이 있으면
+      const todoObj = JSON.parse(todoListFromStorage);
       // console.log(todoObj[todoObj.length-1].id);
-      setTodo(todoObj)
-      setTodoid(todoObj[todoObj.length-1].id);
+      setTodo(todoObj);
+      setTodoid(todoObj[todoObj.length - 1].id);
     }
-  }
+  }, []);
 
-
-  //getTodoList 최초 한번 실행,변수명 todo변경되면 실행된다.
-
-  let deleteTodo = (id)=>{
+  let deleteTodo = (id) => {
     let newTodos = [...todo];
-    let idx = newTodos.findIndex(item=>item.id === id);
-    newTodos.splice(idx,1);
+    let idx = newTodos.findIndex(item => item.id === id);
+    newTodos.splice(idx, 1);
     setTodo(newTodos);
   }
-  let todoUpdate = (id,value)=>{
+
+  let todoUpdate = (id, value) => {
     let newTodos = todo.map(item =>
       item.id === id ? { ...item, checked: value } : item
     );
     setTodo(newTodos);
   }
-  let updateTodo = (id,text)=>{
+
+  let updateTodo = (id, text) => {
     let newTodos = todo.map(item =>
       item.id === id ? { ...item, title: text } : item
     );
     setTodo(newTodos);
   }
-  let todos = todo.map((item,idx)=><Todo key={idx} data={item} deleteTodo={deleteTodo} todoUpdate={todoUpdate} updateTodo={updateTodo}/>);
-  let addTodo = (value)=>{
+
+  let todos = todo.map((item, idx) => <Todo key={idx} data={item} deleteTodo={deleteTodo} todoUpdate={todoUpdate} updateTodo={updateTodo} />);
+
+  let addTodo = (value) => {
     let newtodo = value;
     let newTodos = [...todo];
     let newId = todoid + 1;
     setTodoid(newId);
-    newTodos.push({id:newId, title:newtodo, checked:false})
+    newTodos.push({ id: newId, title: newtodo, checked: false });
     setTodo(newTodos);
     document.querySelector('#todo').value = '';
   }
+
   const setStorage = useCallback(() => {
     const todoStr = JSON.stringify(todo);
     window.localStorage.setItem('todo', todoStr);
   }, [todo]);
-  useEffect(()=>{
-   getTodoList();    
-  },[getTodoList]); //최초 한번만 작동
 
-  useEffect(()=>{
+  useEffect(() => {
+    getTodoList();
+  }, [getTodoList]);
+
+  useEffect(() => {
     setStorage();
-  },[setStorage, todo]);//최초 한번, todo의 값이 변경되면 실행
+  }, [setStorage, todo]);
 
   return (
     <div className="App">
       <h1>React to do list</h1>
-      <Form className="d-flex w-100 align-items-end gap-3" onSubmit={(e)=>{
+      <Form className="d-flex w-100 align-items-end gap-3" onSubmit={(e) => {
         e.preventDefault();
         addTodo(e.target.todo.value);
       }}>
         <Form.Group className="w-100" controlId="todo">
           <Form.Label>할일 입력</Form.Label>
-          <Form.Control type="text" placeholder="입력하세요" />        
+          <Form.Control type="text" placeholder="입력하세요" />
         </Form.Group>
         <Button variant="primary" type="submit">
-         입력
+          입력
         </Button>
       </Form>
-      <hr/>
+      <hr />
       <ul>
         {todos}
       </ul>
-
     </div>
   );
 }
