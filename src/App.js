51564014a01319bbc7e9a912cf6bd244d1f4b546
rock's ezweb <alikerock@gmail.com>
2024-06-11 00:13:57 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,75 +1,69 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useEffect, useState, useCallback } from 'react';
+import { useEffect, useState } from 'react';
 import Todo from './Todo';
 
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 function App() {
-  //window.localStorage.setItem('name', '홍길동');
-  //let test = window.localStorage.getItem('name')
-  //const personObj = JSON.parse(test);  문자열 -> json 형식 객체 변환
-
   const [todo, setTodo] = useState([]);
-  let [todoid, setTodoid] = useState(0);
+  const [todoid, setTodoid] = useState(0);
 
-  const getTodoList = useCallback(() => {
+  const getTodoList = () => {
     const todoListFromStorage = window.localStorage.getItem('todo');
-    //console.log(todoListFromStorage);
-    if (todoListFromStorage !== null && todoListFromStorage !== '[]') { //값이 있으면
+    if (todoListFromStorage !== null && todoListFromStorage !== '[]') { // 값이 있으면
       const todoObj = JSON.parse(todoListFromStorage);
-      // console.log(todoObj[todoObj.length-1].id);
       setTodo(todoObj);
       setTodoid(todoObj[todoObj.length - 1].id);
     }
-  }, []);
+  };
 
-  let deleteTodo = (id) => {
-    let newTodos = [...todo];
-    let idx = newTodos.findIndex(item => item.id === id);
-    newTodos.splice(idx, 1);
+  const setStorage = () => {
+    const todoStr = JSON.stringify(todo);
+    window.localStorage.setItem('todo', todoStr);
+  };
+
+  useEffect(() => {
+    getTodoList();
+  }, []); // 최초 한 번만 실행
+
+  useEffect(() => {
+    setStorage();
+  }, [todo]); // todo가 변경될 때마다 실행
+
+  const deleteTodo = (id) => {
+    const newTodos = todo.filter(item => item.id !== id);
     setTodo(newTodos);
-  }
+  };
 
-  let todoUpdate = (id, value) => {
-    let newTodos = todo.map(item =>
+  const todoUpdate = (id, value) => {
+    const newTodos = todo.map(item =>
       item.id === id ? { ...item, checked: value } : item
     );
     setTodo(newTodos);
-  }
+  };
 
-  let updateTodo = (id, text) => {
-    let newTodos = todo.map(item =>
+  const updateTodo = (id, text) => {
+    const newTodos = todo.map(item =>
       item.id === id ? { ...item, title: text } : item
     );
     setTodo(newTodos);
-  }
+  };
 
-  let todos = todo.map((item, idx) => <Todo key={idx} data={item} deleteTodo={deleteTodo} todoUpdate={todoUpdate} updateTodo={updateTodo} />);
-
-  let addTodo = (value) => {
-    let newtodo = value;
-    let newTodos = [...todo];
-    let newId = todoid + 1;
+  const addTodo = (value) => {
+    const newtodo = value;
+    const newTodos = [...todo];
+    const newId = todoid + 1;
     setTodoid(newId);
     newTodos.push({ id: newId, title: newtodo, checked: false });
     setTodo(newTodos);
     document.querySelector('#todo').value = '';
-  }
+  };
 
-  const setStorage = useCallback(() => {
-    const todoStr = JSON.stringify(todo);
-    window.localStorage.setItem('todo', todoStr);
-  }, [todo]);
-
-  useEffect(() => {
-    getTodoList();
-  }, [getTodoList]);
-
-  useEffect(() => {
-    setStorage();
-  }, [setStorage, todo]);
+  const todos = todo.map((item, idx) => (
+    <Todo key={idx} data={item} deleteTodo={deleteTodo} todoUpdate={todoUpdate} updateTodo={updateTodo} />
+  ));
 
   return (
     <div className="App">
